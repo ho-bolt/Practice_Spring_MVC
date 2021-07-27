18a21e9bad0ea288e3ee5ab7960aed6f160d1be1
ho-bolt <gbolt@naver.com>
2021-07-27 17:37:21 +0900

    Make Member DAO

--- test/src/main/java/com/test/app/member/impl/MemberDAO.java
+++ test/src/main/java/com/test/app/member/impl/MemberDAO.java
@@ -1,5 +1,6 @@
 package com.test.app.member.impl;
 
 public class MemberDAO {
-
+	
+	String sql="select * from member order by mid";
 }
--- test/src/main/java/com/test/app/message/MessageVO.java
+++ test/src/main/java/com/test/app/message/MessageVO.java
@@ -1,5 +1,5 @@
 package com.test.app.message;
 
 public class MessageVO {
-
+	
 }
